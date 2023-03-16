c3eaef62096e7e18784850a2ce90a474355c443f
Daniel Morris <dmorris@morrisdev.com>
2023-03-16 12:31:59 -0700

    Got tables working

---
+++ DCElectricWebAPI/Controllers/StartupController.cs
@@ -0,0 +1,46 @@
+﻿using DCElectricWebAPI.Modules;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace DCElectricWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StartupController : ControllerBase
+    {
+
+        // appJL = getQBApp(2)
+        [HttpGet]
+
+        public async Task<IActionResult> Get() 
+        {
+          
+            //Variables
+            string strTableId = "_DBID_JOBS";
+            int intDtSize = 0;
+            int intCtr = 0;
+          //  DataTable dtJobs = dsJobs.Tables["Jobs_List"];
+
+            var qbc = new QuickBaseConnector();
+            var appJL = qbc.getQBApp(2);
+            Dictionary<string, Intuit.QuickBase.Client.IQTable> dictTables = appJL.GetTables();
+            intDtSize = dictTables.Count;
+            for (intCtr = 0; intCtr < intDtSize; intCtr++)
+            {
+                switch (dictTables.ElementAt(intCtr).Value.ToString())
+                {
+                    case "Jobs":
+                        strTableId = dictTables.ElementAt(intCtr).Key.ToString();
+                        break;
+                    default:
+                        break;
+                }//end switch
+            }//end for
+
+            // gets the table definition
+            var vJobsTable = appJL.GetTable(strTableId);
+            return Ok(vJobsTable);
+        }
+    }
+}
